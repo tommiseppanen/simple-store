94e78689713f1443ae19827df3430dbb669c3911
Tommi Seppänen <tommi.t.seppanen@gmail.com>
2018-01-26 21:05:52 +0200

    Added ItemPanel prefab

---
+++ Assets/Scripts/ItemPanel.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+using Assets.Plugins.SimpleStore;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class ItemPanel : MonoBehaviour {
+
+        [SerializeField]    
+        private Text _name;
+        [SerializeField]
+        private Text _description;
+        [SerializeField]
+        private Text _price;
+        [SerializeField]
+        private Image _image;
+
+        private Item _itemData;
+        public Item ItemData
+        {
+            get
+            {
+                return _itemData;
+            }
+            set
+            {
+                _itemData = value;
+                _name.text = value.Name;
+                _description.text = value.Description;
+                _price.text = value.Price.ToString(CultureInfo.InvariantCulture);
+            }
+        }
+
+        // Use this for initialization
+        void Start () {
+		
+        }
+	
+        // Update is called once per frame
+        void Update () {
+		
+        }
+    }
+}
---
+++ Assets/Scripts/Store.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Assets.Plugins.SimpleStore;
+using Assets.Scripts.Models;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Store : MonoBehaviour {
+
+        [SerializeField]
+        private Transform _itemPanelPrefab;
+
+        private void UpdateSelection()
+        {
+            var random = new System.Random();
+            var generators = new List<IItemGenerator>
+            {
+                new ArmorGenerator(random),
+                new WeaponGenerator(random)
+            };
+            var items = generators.SelectMany(g => g.Generate()).ToList();
+            items.ForEach(i => InitializePanel(Instantiate(_itemPanelPrefab, transform), i));
+        }
+
+        private static void InitializePanel(Component tileObject, Item item)
+        {
+            var panel = tileObject.GetComponent<ItemPanel>();
+            panel.ItemData = item;
+        }
+
+
+        // Use this for initialization
+        void Start ()
+        {
+            UpdateSelection();
+        }
+	
+        // Update is called once per frame
+        void Update () {
+		
+        }
+    }
+}
